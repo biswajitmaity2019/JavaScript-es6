a0624c4773cba0f33ee1c5f08baa4d72ed5c6900
Biswajit Maity <53209725+biswajitmaity2019@users.noreply.github.com>
2019-09-02 01:13:01 +0530

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,33 @@
+let number=[1,2,2,1,8,2,1,4];
+if(number[0]==number[6])
+{
+    console.log("correct");
+}
+
+if(number[0]==number[1])
+{
+    console.log("correct");
+}
+else
+{
+    console.log("wrong try again");
+}
+
+if(number[0]==number[1] && number[1]==number[2])
+{
+    console.log("correct");
+}
+else
+{
+    console.log("wrong try again");
+}
+
+if(number[0]==number[1] || number[1]==number[2])
+{
+    console.log("correct");
+}
+else
+{
+    console.log("wrong try again");
+}
+
