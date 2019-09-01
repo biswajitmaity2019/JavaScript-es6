8e8dbbdb334f5286d0e97bbeb5883d9323a25a9d
Biswajit Maity <53209725+biswajitmaity2019@users.noreply.github.com>
2019-09-02 01:12:12 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,40 @@
+let i=0;
+//While loop
+while(i<5)
+{
+    console.log("The number is "+i);
+    i++;
+}
+//do-while loop
+do{
+    console.log("The number is "+i);
+    i++;
+}while(i<5);
+//for loop
+for (i=0;i<5;i++)
+{
+    console.log("The number is "+i);
+}
+//switch case statement
+let x=prompt("Enter value from 1 to 5",);
+switch(x)
+{
+            case '1':
+            console.log("Enter number is "+x);
+            break;
+            case '2':
+            console.log("Enter number is "+x);
+            break;
+            case '3':
+            console.log("Enter number is "+x);
+            break;
+            case '4':
+            console.log("Enter number is "+x);
+            break;
+            case '5':
+            console.log("Enter number is "+x);
+            break;
+            default:
+            console.log("Enter wrong number is "+x);
+            break;
+}
